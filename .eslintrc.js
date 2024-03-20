7ba725bfea5f9894f01d919d4ec10e6f16a7621b
qingshuang.ye <yeqingshuan@tcsc.com>
2024-03-20 16:42:44 +0800

    代码检测工具Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,11 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
+    // "off"或0 -关闭规则
+    // “warn” 或1 - 开启规则, 使用警告 程序不会退出
+    // "error"或2 - 开启规则, 使用错误 程序退出
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'quotes': 0
   }
 }
