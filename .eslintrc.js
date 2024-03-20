a876336c03ac84fd089fdf236adec7d88463f121
qingshuang.ye <yeqingshuan@tcsc.com>
2024-03-20 16:43:47 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,11 +11,7 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
-    // "off"或0 -关闭规则
-    // “warn” 或1 - 开启规则, 使用警告 程序不会退出
-    // "error"或2 - 开启规则, 使用错误 程序退出
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'quotes': 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
