a2c0dcb7b9eaf1847e6dd61a3bcccb0ed3937a64
qingshuang.ye <yeqingshuan@tcsc.com>
2024-03-20 17:23:08 +0800

    Eslint与Prettier配合解决代码格式问题

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+// 1. VSCode 而言，默认一个 tab 等于 4 个空格，而 ESLint 希望一个 tab 为两个空格
+// 2. 如果大家的 VSCode 安装了多个代码格式化工具的话
+// 3. ESLint 和 Prettier 之间的冲突问题
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  created() {
+    console.log('created')
   }
 }
 </script>
