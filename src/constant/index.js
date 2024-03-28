fd2bc1fca77131e5e35bca144b9014063d3ce747
qingshuang.ye <yeqingshuan@tcsc.com>
2024-03-28 11:29:17 +0800

    410 创建页面组件，使用临时menu菜单

--- src/constant/index.js
+++ src/constant/index.js
@@ -2,5 +2,5 @@ export const TOKEN = 'token'
 // token 时间戳
 export const TIME_STAMP = 'timestamp'
 // 超时时长
-// export const TOKEN_TIMEOUT_VALUE = 2 * 3600 * 1000
-export const TOKEN_TIMEOUT_VALUE = 2 * 1000
+export const TOKEN_TIMEOUT_VALUE = 2 * 3600 * 1000
+// export const TOKEN_TIMEOUT_VALUE = 2 * 1000 // test 2s 超时
