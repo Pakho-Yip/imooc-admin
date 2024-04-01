36371a5c276d9302cd70bd08e21dc2f9d8170d8c
qingshuang.ye <yeqingshuan@tcsc.com>
2024-04-01 14:59:14 +0800

    417 动画逻辑，左侧菜单伸缩功能实现

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="navbar">
+    <!-- 汉堡 -->
+    <Hamburger class="hamburger-container" />
     <div class="right-menu">
       <!-- 头像 -->
       <el-dropdown class="avatar-container" trigger="click">
@@ -34,6 +36,7 @@
 
 <script setup>
 import { useStore } from 'vuex'
+import Hamburger from '@/components/Hamburger/index'
 
 const store = useStore()
 const logout = () => {
@@ -48,6 +51,19 @@ const logout = () => {
   position: relative;
   background-color: #fff;
   box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+
+  .hamburger-container {
+    line-height: 46px;
+    height: 100%;
+    float: left;
+    cursor: pointer;
+    // hover 动画
+    transition: background 0.5s;
+
+    &:hover {
+      background: rgba(0, 0, 0, 0.1);
+    }
+  }
 
   .right-menu {
     display: flex;
