27ff43f194f46c08615b10205b291bd9dd2e1ca2
qingshuang.ye <yeqingshuan@tcsc.com>
2024-04-08 16:57:45 +0800

    5-6 方案落地：自定义语言包国际化处理

---
+++ src/i18n/lang/en.js
@@ -0,0 +1,21 @@
+export default {
+  login: {
+    title: 'User Login',
+    loginBtn: 'Login',
+    usernameRule: 'User name is mandatory',
+    passwordRule: 'The password cannot be less than 6 characters',
+    desc: `
+    Test permission account:<br />
+    Three kinds of accounts are available:<br />
+    1. Super Administrator Account:<br />
+    2. Administrator Account:<br />
+    3. Test configurable accounts:<br />
+    The password is as follows:<br />
+    <br />
+    Import User account:<br />
+    You can log in using the imported user name <br />
+    The password is as follows:123456<br />
+    <b>Note: Import users differentiate between Chinese and English libraries！！！！</b>
+    `
+  }
+}
